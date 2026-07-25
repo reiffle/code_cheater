8c12bc74df5922ce2d01c707faeb6ad3703af450
reiffle <pederreiff@gmail.com>
2026-07-24 22:42:58 -0500

    Added verbose flag.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ if api_key is None:
     raise RuntimeError("No API key found")
 
 from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
 
 def main():
     client = OpenAI(
@@ -17,25 +18,24 @@ def main():
     )
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
-    querry=args.user_prompt
+    messages:list[ChatCompletionMessageParam]= [
+    {"role": "user", "content": args.user_prompt},
+    ]
     response = client.chat.completions.create(
-    model="openrouter/free",
-    messages=[
-        {
-            "role": "user",
-            "content": querry,
-        }
-    ],
+        model="openrouter/free",
+        messages=messages,
     )
     if response.usage==None:
         raise RuntimeError("Unexpected error occurrred")
     prompt=response.usage.prompt_tokens
     complete=response.usage.completion_tokens
-    print(f"User prompt: {querry}")
-    print(f"Prompt tokens: {prompt}")
-    print(f"Response tokens: {complete}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt}")
+        print(f"Response tokens: {complete}")
     print("Response:")
     print(response.choices[0].message.content)
 
